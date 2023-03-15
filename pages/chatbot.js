e49e9362a45d8faed4d5a15a09550edeb5001f89
robriks <80549215+robriks@users.noreply.github.com>
2023-03-14 23:03:41 -0400

    trying gpt-3.5-turbo-0301

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -23,7 +23,7 @@ const Chatbot = () => {
             'https://api.openai.com/v1/completions',
             {
                 prompt: data.prompt,
-                model: 'gpt-3.5-turbo',
+                model: 'gpt-3.5-turbo-0301',
                 max_tokens: 1024,
                 temperature: 0.5
             },
