5ae03865303b2e75eca099b4951a25f175245f11
robriks <marsterlund@gmail.com>
2023-05-30 23:31:06 -0400

    :zap: Convert vestigial img tag for IPFS uploads to nextjs Image
    convert vestigial img tag for IPFS uploads to nextjs Image

--- next.config.js
+++ next.config.js
@@ -3,14 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["replicate.com", "replicate.delivery", "arweave.net"],
+    domains: [
+      "replicate.com",
+      "replicate.delivery",
+      "arweave.net",
+      "horn.infura-ipfs.io",
+    ],
   },
   env: {
     OPENAI_API_TOKEN: `${process.env.OPENAI_API_TOKEN}`,
     INFURA_IPFS_ID: `${process.env.INFURA_IPFS_ID}`,
     INFURA_IPFS_SECRET: `${process.env.INFURA_IPFS_SECRET}`,
-    INFURA_ID: `${process.env.INFURA_ID}`
-  }
+    INFURA_ID: `${process.env.INFURA_ID}`,
+  },
 };
 
 module.exports = nextConfig;
