c32712469ca005f4df74add5874b879e4f63b0ad
robriks <marsterlund@gmail.com>
2023-06-08 02:41:53 -0400

    :lipstick: 2022->2023

--- components/footer.js
+++ components/footer.js
@@ -1,45 +1,35 @@
-import Image from 'next/image';
-import githubLogo from '../public/github-logo.png';
-import twitterLogo from '../public/twitter-logo.png';
-import instagramLogo from '../public/instagram-logo.png';
+import Image from "next/image";
+import githubLogo from "../public/github-logo.png";
+import twitterLogo from "../public/twitter-logo.png";
+import instagramLogo from "../public/instagram-logo.png";
 
 export default function Footer() {
-    return (
-        <div className='justify-center h-auto w-auto'>
-            <div className='justify-center text-xs'>
-                <h2 className='text-center mb-2'>Copyright © 2022 Osterlund Dapp Solutions</h2>
-                <h2 className='text-center font-bold'>Contact:</h2>
-            </div>
+  return (
+    <div className="justify-center h-auto w-auto">
+      <div className="justify-center text-xs">
+        <h2 className="text-center mb-2">
+          Copyright © 2023 Osterlund Dapp Solutions
+        </h2>
+        <h2 className="text-center font-bold">Contact:</h2>
+      </div>
 
-            <div className='flex justify-center'>
-                <div className='p-4'>
-                    <a
-                        href='https://github.com/robriks/HuskyCoin'>
-                        <Image
-                            alt='Github Logo'
-                            src={githubLogo}
-                        />
-                    </a>
-                </div>
-                <div className='p-4'>
-                    <a
-                        href='https://twitter.com/marsterlund'>
-                        <Image
-                            alt='Twitter Logo'
-                            src={twitterLogo}
-                        />
-                    </a>
-                </div>
-                <div className='p-4'>
-                    <a
-                        href='https://instagram.com/marsterlund'>
-                        <Image
-                            alt='Instagram Logo'
-                            src={instagramLogo}
-                        />
-                    </a>
-                </div>
-            </div>
+      <div className="flex justify-center">
+        <div className="p-4">
+          <a href="https://github.com/robriks/HuskyCoin">
+            <Image alt="Github Logo" src={githubLogo} />
+          </a>
         </div>
-    )
+        <div className="p-4">
+          <a href="https://twitter.com/marsterlund">
+            <Image alt="Twitter Logo" src={twitterLogo} />
+          </a>
+        </div>
+        <div className="p-4">
+          <a href="https://instagram.com/marsterlund">
+            <Image alt="Instagram Logo" src={instagramLogo} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
